6bf2af1d3be193fba7b9e620b9772f4dcefa9047
praveenbr111 <praveenreddy.br111@gmail.com>
2025-09-26 01:01:03 +0530

    Subsetting Lists

---
+++ Subsetting Lists/Slicing and dicing.py
@@ -0,0 +1,18 @@
+""" Question :
+
+Use slicing to create a list, downstairs, that contains the first 6 elements of areas.
+Create upstairs, as the last 4 elements of areas. This time, simplify the slicing by omitting the end index.
+Print both downstairs and upstairs using print(). """
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
---
+++ Subsetting Lists/Subsetting lists of lists.py
@@ -0,0 +1,12 @@
+""" Question :
+
+Subset the house list to get the float 9.5. """
+
+house = [["hallway", 11.25],
+         ["kitchen", 18.0],
+         ["living room", 20.0],
+         ["bedroom", 10.75],
+         ["bathroom", 9.50]]
+
+# Subset the house list
+house[4][1]
